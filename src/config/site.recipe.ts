76ef91eab3c225ce8e1ced6e5102ca17efb34ceb
mr-devloper005 <mr.opxoo5@gmail.com>
2026-04-18 00:59:10 +0530

    Configure site tasks and theme

--- src/config/site.recipe.ts
+++ src/config/site.recipe.ts
@@ -2,7 +2,7 @@ import type { SiteRecipe } from '@/design/factory/recipe-types'
 
 export const SITE_RECIPE: SiteRecipe = {
   productFamily: 'editorial',
-  themePack: 'medium-journal',
+  themePack: 'magazine-contrast',
   homepageTemplate: 'article-home',
   navbarTemplate: 'editorial-bar',
   footerTemplate: 'editorial-footer',
@@ -10,7 +10,7 @@ export const SITE_RECIPE: SiteRecipe = {
   primaryTask: 'article',
   enabledTasks: ['article'],
   taskTemplates: {
-    article: 'article-editorial',
+    article: 'article-journal',
   },
   manualOverrides: {
     navbar: false,
